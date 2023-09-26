3adf30b56d1b6c8c164863dbdb9670f3a264d045
Victoria <vicki_ng92@yahoo.com>
2023-09-26 10:37:00 +0100

    error on network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,9 @@
-require("@nomiclabs/hardhat-waffle");
-require("@nomiclabs/hardhat-ethers");
+// require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+const SEPOLIA_URL = process.env.SEPOLIA_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
 const fs = require('fs');
 // const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
 
@@ -12,23 +16,33 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 });
 
 module.exports = {
-  defaultNetwork: "hardhat",
-  networks: {
-    hardhat: {
-      chainId: 1337
-    },
-    goerli: {
-      url: "<YOUR_ALCHEMY_URL>",
-      accounts: [ "<YOUR_PRIVATE_KEY>" ]
-    }
-  },
-  solidity: {
-    version: "0.8.4",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200
+    solidity: "0.8.4",
+    networks: {
+      sepolia: {
+        url: SEPOLIA_URL,
+        accounts: [PRIVATE_KEY]
       }
     }
-  }
-};
+  };
+
+// module.exports = {
+//   defaultNetwork: "hardhat",
+//   networks: {
+//     hardhat: {
+//       chainId: 1337
+//     },
+//     sepolia: {
+//       url: ALCHEMY_URL,
+//       accounts: [PRIVATE_KEY]
+//     }
+//   },
+//   solidity: {
+//     version: "0.8.4",
+//     settings: {
+//       optimizer: {
+//         enabled: true,
+//         runs: 200
+//       }
+//     }
+//   }
+// };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -8,7 +8,7 @@ async function main() {
   const Marketplace = await hre.ethers.getContractFactory("NFTMarketplace");
   const marketplace = await Marketplace.deploy();
 
-  await marketplace.deployed();
+  await marketplace.waitForDeployment();
 
   const data = {
     address: marketplace.address,
